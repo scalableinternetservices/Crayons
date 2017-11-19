93bf2def409125a5570a03f02343c36122089822
eshowa <ehs@ucsb.edu>
2017-11-19 21:53:53 +0000

    fixed annotations in routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   devise_for :user_devises
   resources :profiles
-
+  resources :annotations
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
