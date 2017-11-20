528f79a3c9ee84ef5da68252044f75106239eff9
eshowa <ehs@ucsb.edu>
2017-11-20 04:16:23 +0000

    fixed annotation redirect

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -2,6 +2,12 @@ class AnnotationsController < ApplicationController
  
   def create
     @annotation = Annotation.find_or_create_by(annotation_params)
+    if @annotation.save
+      #if annotation successfully created
+      redirect_to photos_path , notice: "You tagged a photo"
+    else
+      render action: "new"
+    end
     #flash[:notice] = "You tagged this photo!"
     #redirect_to photo_path
   end
