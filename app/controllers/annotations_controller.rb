26c828fb2957991632b6df6abd77e24644136cd0
CarlyLarsson <carly@umail.ucsb.edu>
2017-11-20 17:12:51 -0800

    Tags Fixed
    Bryce helped us fix tags!

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,6 +1,6 @@
 class AnnotationsController < ApplicationController
- 
-  def create
+
+  def update
     @annotation = Annotation.find_or_create_by(annotation_params)
     if @annotation.save
       #if annotation successfully created
@@ -11,23 +11,23 @@ class AnnotationsController < ApplicationController
     #flash[:notice] = "You tagged this photo!"
     #redirect_to photo_path
   end
-  
+
   def index
    @annotations = Annotation.all
   end
-  
+
   def new
     @annotation = Annotation.new
   end
-  
+
   def show
   end
-  
+
   private
 
   #Permitted parameters when creating a photo. This is used for security reasons.
   def annotation_params
     params.require(:annotation).permit(:label)
   end
-  
+
 end
