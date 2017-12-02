4784893974270a2da101207dc6116dcabee97ec4
CarlyLarsson <carly@umail.ucsb.edu>
2017-12-02 01:50:27 -0800

    Created Human Capital
    Created Human Capital

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
     @profile = UserDevise.find(params[:id])
     @photos = Photo.order('created_at')
 
-  end
-
+    @my_human_capital = (@profile.number_of_images_uploaded) + (@profile.number_of_images_downloaded) + (@profile.number_of_labels_made) + (@profile.number_of_upvotes) + (@profile.number_of_downvotes) + (2 * @profile.number_of_upvotes_on_tag) - (2 * @profile.number_of_downvotes_on_tag)
+    @profile.update_column(:human_capital,  @my_human_capital)
+end
 end
