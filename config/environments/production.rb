39f53666efc49564745f3f701b0ef894e8105863
eshowa <ehs@ucsb.edu>
2017-12-03 22:40:24 +0000

    redefinte pclip config path: '

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,17 +77,31 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.paperclip_defaults = {
-    
-    :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
-    :s3_host_name => ENV['S3_HOST_NAME'],
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+#perms 3   
+    storage: :s3,
+    url: ':s3_domain_url',
+    path: ':class/:attachment/:id_partition/:style/:filename',
+    s3_region: 'us-standard',
+    s3_credentials: {
+        bucket: ENV['s3_bucket_name'],
+        access_key_id: ENV['s3_access_id'],
+        secret_access_key: ENV['s3_access_key'],
+        s3_host_name: 's3.amazonaws.com'
     }
-                              
+}
     
+    
+# perms2    
+    # :storage => :s3,
+    # :s3_region => ENV['AWS_REGION'],
+    # :s3_host_name => ENV['S3_HOST_NAME'],
+    # :s3_credentials => {
+    #   :bucket => ENV['S3_BUCKET_NAME'],
+    #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    # }
+                              
+# perms1
 #     storage: :s3,
 #     s3_region: 'us-west-2',
 #      s3_credentials: {
