ed5ae3fce59e39e9850d6d0e3e2c0fe055351a35
CarlyLarsson <carly@umail.ucsb.edu>
2017-12-04 03:00:50 -0800

    Iconized and Paginated
    Iconized and Pagination and cached user object

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,11 +4,14 @@ class ProfilesController < ApplicationController
     Rails.cache.fetch("#{cache_key}/photo", expires_in: 60.min) do
       Photo.where( :user_devises_id => current_user_devise.id )
     end
+    Rails.cache.fetch("#{cache_key}/user", expires_in: 60.min) do
+      UserDevise.where( :user_devises_id => current_user_devise.id )
+    end
   end
 
   def show
     @profile = UserDevise.find(params[:id])
-    @photos = Photo.where( :user_devises_id => current_user_devise.id )
+    @photos = Photo.where( :user_devises_id => current_user_devise.id).paginate(:page => params[:page], :per_page => 5)
 
     @my_human_capital = (@profile.number_of_images_uploaded) + (@profile.number_of_images_downloaded) + (@profile.number_of_labels_made) + (@profile.number_of_upvotes) + (@profile.number_of_downvotes) + (2 * @profile.number_of_upvotes_on_tag) - (2 * @profile.number_of_downvotes_on_tag)
     @profile.update_column(:human_capital,  @my_human_capital)
